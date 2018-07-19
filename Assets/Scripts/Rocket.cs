9a1070a225d8e27011214b1004adbd245bffaf13
Vladyslav <Alksgest@gmail.com>
2018-07-19 16:58:31 +0300

    added rocket & rocket script

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        Vector3 currentPosition = this.transform.position;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(new Vector3(currentPosition.x, currentPosition.y + 10f, currentPosition.z));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(-Vector3.forward);
+        }
+    }
+}
