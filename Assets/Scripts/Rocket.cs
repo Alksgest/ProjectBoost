aa2a5156f8a45382d3a9265fbe7d09a3d4a7794c
Vladyslav <Alksgest@gmail.com>
2018-07-20 10:13:07 +0300

    fixed movement

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    [SerializeField] private float rcsThrust = 100f;
+    [SerializeField] private float mainThrust = 20f;
+
     private Rigidbody rigidBody;
     private AudioSource AudioSource;
+ 
     // Use this for initialization
     void Start()
     {
@@ -17,10 +22,11 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         Vector3 currentPosition = this.transform.position;
 
@@ -28,18 +34,25 @@ public class Rocket : MonoBehaviour
         {
             if (!AudioSource.isPlaying)
                 AudioSource.Play();
-            rigidBody.AddRelativeForce(new Vector3(currentPosition.x, currentPosition.y + 10f, currentPosition.z));
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust); //(new Vector3(currentPosition.x, currentPosition.y + 10f, currentPosition.z)
         }
         else
             AudioSource.Stop();
+    }
+
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
-        {
-            this.transform.Rotate(Vector3.forward);
-        }
+            this.transform.Rotate(Vector3.forward * rotationThisFrame);
+
         else if (Input.GetKey(KeyCode.D))
-        {
-            this.transform.Rotate(-Vector3.forward);
-        }
+            this.transform.Rotate(-Vector3.forward * rotationThisFrame);
+
+        rigidBody.freezeRotation = false;
     }
 }
+
